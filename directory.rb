31054bf3a767f8e7ad702987f0f6ea0fcdfc12e5
mikey-roberts <mikey_roberts@outlook.com>
2021-05-13 19:39:17 +0100

    refactored to loop the students array and remove count variable to replace with count call on students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-#prints the list of students
+# list studetns in an array
+students = [
+  "Dr . Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates" 
+]
+# now let's print the list
 puts "The students of Villains Academy"
-puts "--------------"
-puts "Dr . Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#next lets print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "-------------"
+students.each do |student|
+  puts student
+end
+# finally, we print the total
+puts "Overall, we have #{students.count} great students"
