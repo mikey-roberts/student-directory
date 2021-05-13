91c06cc8d8f546a95a7b4c957b8f2e3d79f5addb
mikey-roberts <mikey_roberts@outlook.com>
2021-05-13 19:19:38 +0100

    number of students added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#prints the list of students
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr . Hannibal Lecter"
@@ -11,3 +12,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#next lets print the total number of students
+puts "Overall, we have 11 great students"
