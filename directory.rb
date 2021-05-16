3fbc1bd1b5c843b9c4305cebae81a4eefb5bfcf2
mikey-roberts <mikey_roberts@outlook.com>
2021-05-16 16:30:13 +0100

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,26 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" 
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header(students)
+      sort_cohort(students)
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -35,8 +58,4 @@ return nil if students.count == 0
 students.count == 1 ? puts("Overall, we have #{students.count} great student") : puts("Overall, we have #{students.count} great students")
 end
 
-students = input_students
-print_header(students)
-sort_cohort(students)
-print(students)
-print_footer(students)
+interactive_menu
